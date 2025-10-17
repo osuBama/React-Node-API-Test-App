e6c4d5043c96e94bb9ccdc5f73553dc6673b3887
NANDo <elamazingfernando@gmail.com>
2025-10-17 21:56:35 +0100

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
 //Lets set up an API for the frontend to talk to the backend
 const express = require('express')
 const app = express()
+
+//CRUDs go here
+app.get("/api", (req, res) =>{
+    res.json()
+})
+
