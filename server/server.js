dd76ba6d169990b48d1ebab37cfb809d5906c93c
NANDo <elamazingfernando@gmail.com>
2025-10-18 15:06:05 +0100

    Scaffolded CRUD on Node server, added environment variables file, added dependencies (dotenv) for further testing

--- server/server.js
+++ server/server.js
@@ -1,9 +1,35 @@
+//Setting up environment variables with dotEnv
+require('dotenv').config();
+
 //Lets set up an API for the frontend to talk to the backend
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+var serverPort = 5000
+
+app.listen(serverPort, () => {console.log("Listening on port " + serverPort)} )
+
+
+//Check if server is alive
+app.get("/alive", (req, res) =>{
+    res.json({ok: true, environment: process.env.NODE_ENV })
+})
 
 //CRUDs go here
 app.get("/api", (req, res) =>{
     res.json()
 })
+
+app.post("/api", (req, res) =>{
+    res.json()
+})
+
+app.put("/api", (req, res) =>{
+    res.json()
+})
+
+app.delete("/api", (req, res) =>{
+    res.json()
+})
+
 
